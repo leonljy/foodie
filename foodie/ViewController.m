06f11a446f415b9a7c63d3991370ebd3b84c82c4
Jeonguk Lee <julee@favorie.co>
2015-09-19 16:29:33 -0700

    Yelp API Integrated

--- foodie/ViewController.m
+++ foodie/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "YPAPISample.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *defaultTerm = @"dinner";
+    NSString *defaultLocation = @"San Francisco, CA";
+    
+    //Get the term and location from the command line if there were any, otherwise assign default values.
+    NSString *term = [[NSUserDefaults standardUserDefaults] valueForKey:@"term"] ?: defaultTerm;
+    NSString *location = [[NSUserDefaults standardUserDefaults] valueForKey:@"location"] ?: defaultLocation;
+    
+    YPAPISample *APISample = [[YPAPISample alloc] init];
+    
+    dispatch_group_t requestGroup = dispatch_group_create();
+    
+    dispatch_group_enter(requestGroup);
+
+    [APISample queryTopBusinessInfoForTerm:term location:location completionHandler:^(NSDictionary *topBusinessJSON, NSError *error) {
+        
+        if (error) {
+            NSLog(@"An error happened during the request: %@", error);
+        } else if (topBusinessJSON) {
+            NSLog(@"Top business info: \n %@", topBusinessJSON);
+        } else {
+            NSLog(@"No business was found");
+        }
+        
+        dispatch_group_leave(requestGroup);
+    }];
+//    
+    dispatch_group_wait(requestGroup, DISPATCH_TIME_FOREVER); // This avoids the program exiting before all our asynchronous callbacks have been made.
 }
 
 - (void)didReceiveMemoryWarning {
